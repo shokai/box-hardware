85f2f074eb6e0e3752047654195cc60afb45e84c
shokai <hashimoto@shokai.org>
2010-07-23 03:58:35 +0900

    LED

--- box_slave/box_slave/box_slave/main.c
+++ box_slave/box_slave/box_slave/main.c
@@ -1,12 +1,23 @@
-//----------------------------------------------------------------------------
-// C main line
-//----------------------------------------------------------------------------
+// box
+// CY8C29466
 
 #include <m8c.h>        // part specific constants and macros
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
 
+#define _BV(BIT) (1<<BIT)
+#define sbi(BYTE,BIT) (BYTE |= _BV(BIT))
+#define cbi(BYTE,BIT) (BYTE &= ~_BV(BIT))
+
+#define LED_ON() sbi(PRT2DR, 0) // LED
+#define LED_OFF() cbi(PRT2DR, 0)
+#define BTN_PORT PRT2DR // push button
+#define BTN_BIT _BV(2)
+
 
 void main(void)
 {
-    // Insert your main routine code here.
+    LED_ON();
+    for(;;){
+        
+    }
 }
