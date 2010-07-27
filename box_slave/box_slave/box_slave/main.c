264ce6709f61fe7ff053da413fd9c4cbf417bc21
shokai <hashimoto@shokai.org>
2010-07-28 05:52:00 +0900

    get 4 adc

--- box_slave/box_slave/box_slave/main.c
+++ box_slave/box_slave/box_slave/main.c
@@ -1,8 +1,13 @@
-// box
+// box slave
+// work as I2C slave
+// get weight by 4 ADC (PORT0 PIN 0,2,4,6)
+// UART TX for debug
 // CY8C29466
 
 #include <m8c.h>        // part specific constants and macros
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
+#include <string.h>
+#include <stdlib.h>
 
 #define _BV(BIT) (1<<BIT)
 #define sbi(BYTE,BIT) (BYTE |= _BV(BIT))
@@ -10,23 +15,16 @@
 
 #define LED_ON() sbi(PRT2DR, 1) // LED
 #define LED_OFF() cbi(PRT2DR, 1)
-#define LED_DBG_ON() sbi(PRT2DR, 0) // LED
+#define LED_DBG_ON() sbi(PRT2DR, 0) // debug LED
 #define LED_DBG_OFF() cbi(PRT2DR, 0)
 #define BTN_PORT PRT2DR // push button
 #define BTN_BIT _BV(2)
 
+int get_adc(BYTE amux_channel);
 int ad;
-
-// AMUX4_PORT0_0 => 0x00
-// AMUX4_PORT0_2 => 0x01
-// AMUX4_PORT0_4 => 0x02
-// AMUX4_PORT0_6 => 0x03
-int get_adc(BYTE amux_pin){
-    AMUX4_InputSelect(amux_pin);
-    ADCINC_GetSamples(0);
-    while(!ADCINC_fIsDataAvailable());
-    return ADCINC_iClearFlagGetData();
-}
+BYTE ad_pin;
+int weights[4];
+char buf[6];
 
 void main(void)
 {
@@ -34,8 +32,30 @@ void main(void)
     AMUX4_Start();
     PGA_1_Start(PGA_1_HIGHPOWER);
     ADCINC_Start(ADCINC_HIGHPOWER);
+    TX8_Start(TX8_PARITY_NONE);
+
+    TX8_CPutString("start");
+    TX8_PutCRLF();
     LED_DBG_ON();
     for(;;){
-        ad = get_adc(0);
+        for(ad_pin = 0; ad_pin < 4; ad_pin++){
+            ad = get_adc(ad_pin);
+            weights[ad_pin] = ad;
+            TX8_PutChar(ad_pin);
+            TX8_CPutString(":");
+            TX8_PutString(itoa(buf, ad, 6));
+            TX8_PutCRLF();
+        }
     }
+}
+
+// AMUX4_PORT0_0 => 0x00
+// AMUX4_PORT0_2 => 0x01
+// AMUX4_PORT0_4 => 0x02
+// AMUX4_PORT0_6 => 0x03
+int get_adc(BYTE amux_channel){
+    AMUX4_InputSelect(amux_channel);
+    ADCINC_GetSamples(0);
+    while(!ADCINC_fIsDataAvailable());
+    return ADCINC_iClearFlagGetData();
 }
